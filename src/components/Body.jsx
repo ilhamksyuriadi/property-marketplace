759349069439238c289782c2fb906a053012f6cb
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-08-03 17:03:39 +0700

    adding summary at body

---
+++ src/components/Body.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Body.scss';
+
+const Body = () => {
+    return (
+        <div className="body-container">
+            <div className="summary-container">
+                <div>
+                    <h1>Start Your Journey</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat
+                    </p>
+                </div>
+            </div>
+            <div className="card-container">
+                <h1>card container</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Body
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Main.scss';
 import Header from '../components/Header';
+import Body from '../components/Body';
 
 const Main = () => {
     return (
         <div className="main-container">
             <Header></Header>
+            <Body></Body>
         </div>
     )
 }
